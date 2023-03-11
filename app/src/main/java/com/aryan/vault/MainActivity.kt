da3b41c39701db9948a7b194e7ac26ea063d85ce
aryan <aryanrdat@gmail.com>
2023-03-11 13:26:01 +0530

    Version 2

--- app/src/main/java/com/aryan/vault/MainActivity.kt
+++ app/src/main/java/com/aryan/vault/MainActivity.kt
@@ -2,10 +2,42 @@ package com.aryan.vault
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.viewpager.widget.ViewPager
+import com.aryan.vault.Adapter.MainAdapter
+import com.aryan.vault.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager: ViewPager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        tabLayout = binding.tabLayout
+        viewPager = binding.viewPager
+
+        tabLayout.addTab(tabLayout.newTab().setText("Images"))
+        tabLayout.addTab(tabLayout.newTab().setText("Pdfs"))
+        tabLayout.tabGravity = TabLayout.GRAVITY_FILL
+
+        val adapter = MainAdapter(this,supportFragmentManager, tabLayout.tabCount)
+        viewPager.adapter = adapter
+
+        viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+               viewPager.currentItem = tab!!.position
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
     }
 }
